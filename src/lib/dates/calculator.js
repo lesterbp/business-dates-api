93a0e41e584cddb20c517c353e20acec3491943c
Lester.Prudente <lester_b_p@yahoo.com>
2019-09-03 21:40:35 +0800

    SP-1: added core logic and adjusted test

--- src/lib/dates/calculator.js
+++ src/lib/dates/calculator.js
@@ -1,8 +1,69 @@
-exports.settlementDate = (initialDate, delay) => {
-  return {
-    businessDate: '2018-11-15T10:10:10Z',
-    totalDays: 6,
-    weekendDays: 2,
-    holidayDays: 1,
+const bizniz = require('bizniz').default
+const { DateTime } = require('luxon')
+const { getLogger } = require('../logging/logger')
+const { holidays } = require('../config/holidays')
+
+const TIME_FORMAT = "yyyy-LL-dd'T'HH:mm:ss'Z'"
+const TIME_ZONE = 'UTC'
+
+const numberOfHolidays = (
+  startDate, endDate, locale = 'america',
+) => holidays.reduce((total, holiday) => {
+  if (startDate <= holiday.date && endDate >= holiday.date
+    && holiday.locale === locale.toLowerCase()) {
+    return total + 1
+  }
+
+  return total
+}, 0)
+
+const adjustDateForHolidays = (startDate, endDate, previousHolidayDays = 0) => {
+  const holidaysDays = numberOfHolidays(startDate, endDate)
+  const adjustmentNeeded = holidaysDays - previousHolidayDays
+
+  if (adjustmentNeeded > 0) {
+    let newEndDate = bizniz.addWeekDays(endDate.toJSDate(), adjustmentNeeded)
+    newEndDate = DateTime.fromJSDate(newEndDate).setZone(TIME_ZONE)
+
+    // doing recursive call in case there's new holiday when we added weekdays
+    return adjustDateForHolidays(startDate, newEndDate, holidaysDays)
+  }
+
+  return endDate
+}
+
+exports.settlementDate = (initialDate, delay = 0) => {
+  const log = getLogger()
+
+  try {
+    log.debug('settlementDate: starting calculation', {
+      params: { initialDate, delay },
+    })
+
+    const luxInitDate = DateTime.fromISO(initialDate)
+    if (!luxInitDate.isValid) {
+      throw new Error('invalid initialDate')
+    }
+
+    const isStartDateWeekend = bizniz.isWeekendDay(luxInitDate.toJSDate())
+    const weekendAdjustment = isStartDateWeekend ? 1 : 0
+    const adjustedDelay = delay - 1 + weekendAdjustment // delays include the current day
+
+    const resultDate = bizniz.addWeekDays(luxInitDate.toJSDate(), adjustedDelay)
+    let luxResultDate = DateTime.fromJSDate(resultDate).setZone(TIME_ZONE)
+    luxResultDate = adjustDateForHolidays(luxInitDate, luxResultDate)
+
+    const weekendDays = bizniz.weekendDaysBetween(luxInitDate.toJSDate(), luxResultDate.toJSDate())
+    const totalDays = luxResultDate.diff(luxInitDate, 'days').days + 1 // to include current day
+    const holidayDays = numberOfHolidays(luxInitDate, luxResultDate)
+    return {
+      businessDate: luxResultDate.toFormat(TIME_FORMAT),
+      totalDays,
+      weekendDays,
+      holidayDays,
+    }
+  } catch (e) {
+    log.error('settlementDate: encountered error, returning null', { errorMessage: e.message })
+    return null
   }
 }
--- test/unit/lib/dates/calculator.spec.js
+++ test/unit/lib/dates/calculator.spec.js
@@ -27,9 +27,22 @@ describe('#settlementDate', () => {
   it('calculates long delays with weekend and holiday', () => {
     const result = settlementDate('2018-12-25T00:00:00Z', 20)
     expect(result).to.be.not.null
-    expect(result.businessDate).to.be.equal('2019-01-18T00:00:00Z')
-    expect(result.totalDays).to.be.equal(30)
+
+    /* 
+    Needed to adjust the expected results from the exam requirement
+    as it is impossible to get result date of 2019-01-18
+    if you add 20 business days
+    that would also skip weekend in the count
+    and there are 3 holidays based on the calendar given
+    */
+    expect(result.businessDate).to.be.equal('2019-01-24T00:00:00Z')
+    expect(result.totalDays).to.be.equal(31)
     expect(result.weekendDays).to.be.equal(8)
-    expect(result.holidayDays).to.be.equal(2)
+    expect(result.holidayDays).to.be.equal(3)
+  })
+
+  it('should return null in case of invalid date', () => {
+    const result = settlementDate('2018-12-25T00:00:00ABCD', 20)
+    expect(result).to.be.null
   })
 })
