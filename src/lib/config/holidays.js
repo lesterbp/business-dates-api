8a221b9d76b47632d43898429b1ed46d86a275d6
Lester.Prudente <lester_b_p@yahoo.com>
2019-09-03 21:40:05 +0800

    SP-1: updated configs

---
+++ src/lib/config/holidays.js
@@ -0,0 +1,24 @@
+const { DateTime } = require('luxon')
+
+exports.holidays = [
+  { date: DateTime.fromISO('2018-01-01T00:00:00Z'), locale: 'america' },
+  { date: DateTime.fromISO('2018-01-15T00:00:00Z'), locale: 'america' },
+  { date: DateTime.fromISO('2018-02-19T00:00:00Z'), locale: 'america' },
+  { date: DateTime.fromISO('2018-05-28T00:00:00Z'), locale: 'america' },
+  { date: DateTime.fromISO('2018-06-04T00:00:00Z'), locale: 'america' },
+  { date: DateTime.fromISO('2018-09-03T00:00:00Z'), locale: 'america' },
+  { date: DateTime.fromISO('2018-10-08T00:00:00Z'), locale: 'america' },
+  { date: DateTime.fromISO('2018-11-12T00:00:00Z'), locale: 'america' },
+  { date: DateTime.fromISO('2018-11-22T00:00:00Z'), locale: 'america' },
+  { date: DateTime.fromISO('2018-11-23T00:00:00Z'), locale: 'america' },
+  { date: DateTime.fromISO('2018-12-25T00:00:00Z'), locale: 'america' },
+  { date: DateTime.fromISO('2019-01-01T00:00:00Z'), locale: 'america' },
+  { date: DateTime.fromISO('2019-01-21T00:00:00Z'), locale: 'america' },
+  { date: DateTime.fromISO('2019-02-18T00:00:00Z'), locale: 'america' },
+  { date: DateTime.fromISO('2019-05-27T00:00:00Z'), locale: 'america' },
+  { date: DateTime.fromISO('2019-06-04T00:00:00Z'), locale: 'america' },
+  { date: DateTime.fromISO('2019-09-02T00:00:00Z'), locale: 'america' },
+  { date: DateTime.fromISO('2019-11-11T00:00:00Z'), locale: 'america' },
+  { date: DateTime.fromISO('2019-11-28T00:00:00Z'), locale: 'america' },
+  { date: DateTime.fromISO('2019-12-25T00:00:00Z'), locale: 'america' },
+]
--- src/routes.js
+++ src/routes.js
@@ -5,7 +5,7 @@ exports.setupRoute = (app) => {
   const BASE_PATH = process.env.API_BASE_PATH
   log.info('routes: setting base path', { basePath: BASE_PATH })
 
-  app.get(`${BASE_PATH}/settlement`, (req, res) => {
+  app.get(`${BASE_PATH}/getBusinessDateWithDelay`, (req, res) => {
     res.json({ sucess: true })
   })
 }
